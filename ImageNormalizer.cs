6a40e49d0b3f98dc3f160224b63cbf114dc9e94a
Gabriel Howat <gabrielhowat@hotmail.com>
2023-07-28 19:06:43 -0300

    Update

--- ImageNormalizer.cs
+++ ImageNormalizer.cs
@@ -4,6 +4,7 @@ using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using static System.Net.Mime.MediaTypeNames;
+using System.Drawing.Imaging;
 
 namespace ImageNormalizerLibrary
 {
@@ -17,7 +18,7 @@ namespace ImageNormalizerLibrary
 
         private Mat originalImage = new();
 
-        private Mat outputImage = new();
+        public Mat outputImage;
 
         public ImageNormalizer(string base64, string newFileName, string newFilePath)
         {
@@ -47,7 +48,7 @@ namespace ImageNormalizerLibrary
                 CvInvoke.CvtColor(originalImage, grayscaleImage, ColorConversion.Bgr2Gray);
 
                 Mat binaryImage = new Mat();
-                CvInvoke.Threshold(grayscaleImage, binaryImage, 200, 255, ThresholdType.Binary);
+                CvInvoke.Threshold(grayscaleImage, binaryImage, 255, 255, ThresholdType.Binary);
 
                 grayscaleImage.Dispose();
 
@@ -55,26 +56,44 @@ namespace ImageNormalizerLibrary
             }
         }
 
-        private async Task<Mat> PreProcessImageBase64()
+        private async Task<Mat> PreProcessImageBase64(int newHeight)
         {
             if (ImageBase64 is null)
                 return new();
 
             byte[] imageBytes = Convert.FromBase64String(ImageBase64);
 
-            // Create a MemoryStream from the byte array
+            // Load the image using a MemoryStream
+
+            // Convert the Bitmap to an Emgu.CV image (Image<Bgr, byte>)
             await Task.Run(() => CvInvoke.Imdecode(imageBytes, ImreadModes.Color, originalImage));
+
+            originalImage = ResizeMatByHeight(originalImage, newHeight);
 
             Mat grayscaleImage = new Mat();
             CvInvoke.CvtColor(originalImage, grayscaleImage, ColorConversion.Bgr2Gray);
 
             Mat binaryImage = new Mat();
             CvInvoke.Threshold(grayscaleImage, binaryImage, 200, 255, ThresholdType.Binary);
-
-            originalImage.Dispose();
+            //outputBitmap = BitmapExtension.ToBitmap(binaryImage);
             grayscaleImage.Dispose();
 
             return binaryImage;
+        }
+
+        private Mat ResizeMatByHeight(Mat originalMat, int desiredHeight)
+        {
+            int originalWidth = originalMat.Width;
+            int originalHeight = originalMat.Height;
+            double aspectRatio = (double)originalWidth / originalHeight;
+
+            int calculatedWidth = (int)(desiredHeight * aspectRatio);
+
+            // Resize the Mat using the calculated width and desired height while preserving the aspect ratio.
+            Mat resizedMat = new Mat();
+            CvInvoke.Resize(originalMat, resizedMat, new Size(calculatedWidth, desiredHeight), 0, 0, Inter.Linear);
+
+            return resizedMat;
         }
 
         private async Task<Rectangle> GetBoundingRect(Mat image)
@@ -87,31 +106,27 @@ namespace ImageNormalizerLibrary
             // Find the bounding box using parallel loops
             await Task.Run(() =>
             {
-                object lockObj = new object();
                 Parallel.For(0, image.Rows, y =>
                 {
                     for (int x = 0; x < image.Cols; x++)
                     {
                         if (image.GetRawData(y, x)[0] == 0) // Black pixel found
                         {
-                            lock (lockObj)
-                            {
-                                if (top == -1 || y < top)
-                                    top = y;
-                                if (bottom == -1 || y > bottom)
-                                    bottom = y;
-                                if (left == -1 || x < left)
-                                    left = x;
-                                if (right == -1 || x > right)
-                                    right = x;
-                            }
+                            if (top == -1 || y < top)
+                                top = y;
+                            if (bottom == -1 || y > bottom)
+                                bottom = y;
+                            if (left == -1 || x < left)
+                                left = x;
+                            if (right == -1 || x > right)
+                                right = x;
                         }
                     }
                 });
             });
 
             // Get the region of interest (ROI) based on the bounding box
-            var boundingBox =  new Rectangle(left, top, right - left, bottom - top);
+            var boundingBox = new Rectangle(left, top, right - left, bottom - top);
 
             boundingBox.X = Math.Max(boundingBox.X, 0);
             boundingBox.Y = Math.Max(boundingBox.Y, 0);
@@ -136,11 +151,10 @@ namespace ImageNormalizerLibrary
 
                 // Save the byte array to the MemoryStream
                 ms.Write(imageBytes, 0, imageBytes.Length);
+                var array = ms.ToArray();
 
                 // Convert the MemoryStream to a base64 string
-                string base64Image = Convert.ToBase64String(ms.ToArray());
-
-                return base64Image;
+                return Convert.ToBase64String(array);
             }
         }
 
@@ -149,13 +163,13 @@ namespace ImageNormalizerLibrary
             Mat binaryImage;
 
             if (ImageBase64 != null)
-                binaryImage = await PreProcessImageBase64();
+                binaryImage = await PreProcessImageBase64(finishSize);
             else
                 binaryImage = await PreProcessImagePath();
 
 
             Rectangle boundingBox = await GetBoundingRect(binaryImage);
-           
+
 
             // Get the region of interest (ROI) based on the adjusted bounding box
             using (Mat roiImage = new Mat(originalImage, boundingBox))
@@ -173,19 +187,20 @@ namespace ImageNormalizerLibrary
 
                     // Calculate the padding required to center the resized roiImage on the white background
                     int paddingX = (finishSize - resizedImage.Width) / 2;
+                    if (paddingX <= 0) paddingX = 0;
                     int paddingY = (finishSize - resizedImage.Height) / 2;
+                    if (paddingY <= 0) paddingY = 0;
 
                     // Create a new white background
-                    using (Mat whiteBackground = new Mat(finishSize, finishSize, originalImage.Depth, originalImage.NumberOfChannels))
-                    {
-                        whiteBackground.SetTo(new MCvScalar(255, 255, 255)); // Set all pixels to white
+                    Mat whiteBackground = new Mat(finishSize, finishSize, originalImage.Depth, originalImage.NumberOfChannels);
+                    whiteBackground.SetTo(new MCvScalar(255, 255, 255)); // Set all pixels to white
 
-                        // Paste the resized roiImage onto the white background
-                        CvInvoke.CopyMakeBorder(resizedImage, whiteBackground, paddingY, paddingY, paddingX, paddingX, BorderType.Constant, new MCvScalar(255, 255, 255));
+                    // Paste the resized roiImage onto the white background
+                    CvInvoke.CopyMakeBorder(resizedImage, whiteBackground, paddingY, paddingY, paddingX, paddingX, BorderType.Constant, new MCvScalar(255, 255, 255));
 
-                        // Save the final image to the specified output path
-                        outputImage = whiteBackground;
-                    }
+                    // Save the final image to the specified output path
+                    outputImage = whiteBackground;
+                    //outputBitmap = BitmapExtension.ToBitmap(outputImage);
                 }
             }
         }
